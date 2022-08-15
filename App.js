93410b64e32fea5a899b55279ed85e561fbbd4e4
Andy Foster <andrewfosteris@gmail.com>
2022-08-15 14:52:23 +1200

    Add styling to Text element

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/react-in-jsx-scope */
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -5,6 +8,7 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.header}>Hello World!!!</Text>
       <Button title="click me" />
+      <Text style={styles.paragraphText}>More text here</Text>
     </View>
   );
 }
@@ -19,6 +23,13 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 30,
     fontWeight: 'bold',
-    color: '#dd00ff',
+    color: '#eeff00',
+    backgroundColor: 'black',
+  },
+  paragraphText: {
+    padding: 10,
+    margin: 50,
+    borderWidth: 1,
+    borderColor: 'blue',
   },
 });
